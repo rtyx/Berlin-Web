17443fd224532c8bdf09053fb3575b765d3d6e46
rtyx <rtyx@MacBook-Pro-de-Rafael.local>
2016-09-07 15:44:32 +0200

    Added Hamburger Menu

---
+++ script.js
@@ -0,0 +1,28 @@
+
+document.getElementById("menu").addEventListener('click', function menuclick(event) {
+    document.getElementById("myDropdown").classList.toggle("show");
+    event.stopPropagation();
+});
+
+document.addEventListener('click', function exitmenu(event) {
+    if (!event.target.closest('#myDropdown')) {
+        document.getElementById("myDropdown").classList.remove("show");
+    }
+});
+
+/*  IN PROCESS
+
+document.addEventListener('click', function exitmenu(event) {
+    var clicked = event.target;
+    function checkParents(clicked) {
+      var child = clicked;
+        if (child.id == "myDropdown") {
+          return;
+        } else {
+          checkParents(child.parentNode);
+        }
+    }
+    checkParents(clicked);
+});
+
+*/
